ba3a7e059c3f944643f77441c13a34bcbdcaa295
daniel-chowdhury <danielfchowdhury@gmail.com>
2022-08-19 13:18:10 -0400

    fixed an error, moved the bot button click part to the third test

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -23,17 +23,17 @@ test('Check to see if draw button shows card options', async () => {
     let draw_btn = await driver.findElement(By.id('draw'))
     await driver.sleep(3000)
     await draw_btn.click()
-    await driver.sleep(3000)
-    let bot_btn = await driver.findElement(By.className('bot-btn'))
-    //console.log("dfkjghdfghj" + bot_btn)
-    await bot_btn[1].click()
+    
     let card = await driver.findElement(By.xpath('//section/div'))
     let displayed = await card.isDisplayed()
     expect(displayed).toBe(true)
 })
 
 test('Check to see if Add to Duo button works', async () => {
-    
+    await driver.sleep(3000)
+    let bot_btn = await driver.findElement(By.className('bot-btn'))
+    //console.log("dfkjghdfghj" + bot_btn)
+    await bot_btn[1].click()
     await driver.sleep(5000)
     let your_duo = await driver.findElement(By.id('player-duo'))
     let displayed = await your_duo.isDisplayed()
